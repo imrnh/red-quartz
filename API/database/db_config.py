9e44d65e2645b14460186decaae898e8e547c972
sugarcatx <mu.imran2002@gmail.com>
2022-08-07 02:25:18 +0600

    Swtiched db to mysql

--- API/database/db_config.py
+++ API/database/db_config.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345678@localhost/TIME_TRACKER"   
-#postgresql://user:password@postgresserver/db
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345678@localhost/TIME_TRACKER"  # --> Postgres
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://redq:12345678@localhost:3306/TIME_TRACKER"  # MYSQL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, echo=True
